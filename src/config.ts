3dedfda87e8a98a914c532b44768e5baea527273
kemi-20 <yuminghao805@gmail.com>
2026-06-12 10:37:07 +0800

    Add HOMOS site icon

--- src/config.ts
+++ src/config.ts
@@ -30,12 +30,21 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
-		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+			src: "/favicon/homos.svg",
+		},
+		{
+			src: "/favicon/homos-32.png",
+			sizes: "32x32",
+		},
+		{
+			src: "/favicon/homos-180.png",
+			sizes: "180x180",
+		},
+		{
+			src: "/favicon/homos-192.png",
+			sizes: "192x192",
+		},
 	],
 };
 
